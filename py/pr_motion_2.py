ab4a5cf2ad9235f0f5ca7fe0748297b4797aa54f
alessandro <agileontheweb@gmail.com>
2018-08-29 19:54:45 +0200

    Update pr_motion_2.py

--- py/pr_motion_2.py
+++ py/pr_motion_2.py
@@ -10,14 +10,13 @@ def my_callback(channel):
     # Here, alternatively, an application / command etc. can be started.
     print('There was a movement!')
     GPIO.output(24,GPIO.HIGH)
+    time.sleep(2) 
+    GPIO.output(24,GPIO.LOW)
 try:
     GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=my_callback)
     while True:
       time.sleep(1)
     
-    time.sleep(1) 
-    GPIO.output(24,GPIO.LOW)
-
 except KeyboardInterrupt:
   print "Finish..."
   GPIO.cleanup()
