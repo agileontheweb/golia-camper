39d109c219790678e4153032dc7c6b0a9c49ce29
alessandro <agileontheweb@gmail.com>
2018-08-29 19:02:44 +0200

    Update pir_motion_sensor.py

--- py/pir_motion_sensor.py
+++ py/pir_motion_sensor.py
@@ -16,8 +16,7 @@ try:
             print("Motion Detected...")
             time.sleep(5) #to avoid multiple detection
         time.sleep(0.1) #loop delay, should be less than detection delay
-        else:
-            print("Stop Motion...")
+        print("Stop Motion...")
 
 except:
     GPIO.cleanup()
