c1e6085af089654212cb3ff96447c275803922d4
alessandro <agileontheweb@gmail.com>
2018-08-29 19:27:50 +0200

    Update pir_motion_sensor.py

--- py/pir_motion_sensor.py
+++ py/pir_motion_sensor.py
@@ -2,20 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN) #PIR
 GPIO.setup(24, GPIO.OUT) #BUzzer
 
-try:
-  time.sleep(1) # to stabilize sensor
-   while True:
-      if GPIO.input(23):
-         GPIO.output(24, True)
-          print("Motion Detected...")
-          time.sleep(0.1) #Buzzer turns on for 0.5 sec
-       time.sleep(0) #loop delay, should be less than detection delay
-       print("Stop Motion...")
-       GPIO.output(24, False)
-except:
-  GPIO.cleanup()
+pirPin = 23
+GPIO.setup(pirPin, GPIO.IN)
 
 
+while True:
+    if GPIO.input(pirPin) == GPIO.LOW
+         print "Motion detected!"
+    else:
+         print "No motion"
+    time.sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
