740e23868b5b248bb34e18e6b806647f3acd9d8b
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-08-30 17:10:48 +0200

    text

--- py/ultrasonic-distance.php
+++ py/ultrasonic-distance.php
@@ -1,5 +1,5 @@
 <?php
-  $myfile = fopen("ulstrasonic-distance.txt", "r") or die("Unable to open file!");
+  $myfile = fopen("ultrasonic-distance.txt", "r") or die("Unable to open file!");
   echo fread($myfile,filesize("ulstrasonic-temp.txt"));
   fclose($myfile);
 ?>
