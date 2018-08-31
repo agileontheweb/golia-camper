abda55cbc72dd8b24808a2e18db6f4480dc95e3c
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-08-31 16:59:14 +0200

    temperature.js

--- js/temperature.js
+++ js/temperature.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
 
+  loadFUlstrasonicSensor();
+
   setInterval(function(){
      loadFile();
   }, 1000);
 
   function loadFile(){
-    console.log("ok");
     $("#temperature-cpu").load('py/monitor-temp.php');
+    console.log("temperature-cpu");
+
   }
 
   function loadFUlstrasonicSensor(){
     $("#ulstrasonic-sensor").load('py/ulstrasonic-distance.php');
+    console.log("ulstrasonic-sensor");
   }
 
 });
