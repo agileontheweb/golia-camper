ff3b106bfacb15a5a62de5bd7af07bd2477bc49a
Alessandro Cuoghi <agileontheweb@gmail.com>
2019-03-29 18:41:04 +0100

    structure

--- index.php
+++ index.php
@@ -1,85 +1,140 @@
 <!doctype html>
-
 <?php
-$command = escapeshellcmd('py/monitor-temp.py');
-$output = shell_exec($command);
-echo $output;
-
+  $command = escapeshellcmd('py/monitor-temp.py');
+  $output = shell_exec($command);
+  echo $output;
 ?>
-
-
+<!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Golia Camper Dashboard</title>
-    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="scss/layout.css" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/css/bootstrap4-toggle.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <title>GOLIATH - Panel control</title>
   </head>
   <body>
-    <main role="main">
-      <div class="container">
-        <!-- Example row of columns -->
-        <div class="row">
-          <div class="col-sm  -4">
-            <h2>Temperature Cpu Raspberry</h2>
-            <div id="temperature-cpu"></div>
-            <!-- <form action = "py/monitor-temp.py" method = "POST">
-              <input type = "submit" value = "Run the Program!!!">
-            </form> -->
 
-          </div>
-
-          <div class="col-sm  -4">
-            <div class="googlemap">
-              <h2>Retro</h2>
-              <p>
-                Quando inserisco la retro si attiva la telecamera dietro
-                <div class="camper-top-view">
-                  <img src=""/>
-                  <div class="sensor-back-center">
-                    <?#php
-                      #$temp = exec('py/ultrasonic_distance.py');
-                      #echo "$temp test";
-                    #?>
-
-                    <div id="ulstrasonic-sensor"></div>
-
-                  </div>
-                </div>
-                <br />
-                o diventa conta km
-              </p>
-            </div>
-          </div>
-
-          <div class="col-sm  -4">
-
-            <div class="googlemap">
-              <h2>Mappa</h2>
-            </div>
-
-            <?php
-              $command = escapeshellcmd('py/led.py');
-              $output = shell_exec($command);
-              echo $output;
-              echo "ss"
-            ?>
-          </div>
-          <!-- <div class="col-md-4">
-            <h2>Giveme position</h2>
-            <div id="map"></div>
-            <p id="demo"></p>
-            <p><a class="btn btn-secondary" href="#" id="getPosition" role="button">position</a></p>
-          </div> -->
+    <div class="container">
+      <div class="row align-items-center py-2 bg-dark text-white">
+        <div class="col-4">
+          <small>
+            <i class="fas fa-car-battery"></i> Bs 12 V
+          </small>
         </div>
-        <hr>
+        <div class="col-4">
+          <small>
+            <i class="fas fa-car-battery"></i> Bm 12 V
+          </small>
+        </div>
+        <div class="col-4">
+          <small>
+            <i class="fas fa-thermometer-full"></i> Rasp 23
+          </small>
+        </div>
       </div>
-    </main>
-    <?php include 'shared/footer.php' ?>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBI7LlKH8pWqfJgvOJX9Eshaa2UfNC0McE"></script>
+      <div class="row align-items-center bg-light py-3 border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="fas fa-bolt"></i> <span class="ml-2">12 V</span> </p>
+        </div>
+        <div class="col-4 text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="fas fa-plug"></i> <span class="ml-2">220 V </span></p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="fas fa-shower"></i> Acqua</p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="fas fa-lightbulb"></i> Tavolo</p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+
+      <div class="py-3">
+        <h4>Led control</h4>
+      </div>
+
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="far fa-lightbulb"></i>Led Tavolo</p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="far fa-lightbulb"></i>Led Cucina</p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="far fa-lightbulb"></i>Led Divanetti</p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+
+      <div class="row align-items-center bg-light py-3 by border-top">
+        <div class="col-8">
+          <p class="align-middle"><i class="far fa-lightbulb"></i>Led Portellone</p>
+        </div>
+        <div class="col-4 bg-light text-right">
+          <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+        </div>
+      </div>
+
+      <div class="py-3">
+        <h4>Siurezza</h4>
+        <div class="row align-items-center bg-light py-3 by border-top">
+          <div class="col-8">
+            <p class="align-middle"><i class="fas fa-map-marker-alt"></i> <span class="ml-2">Posizione</span> </p>
+          </div>
+          <div class="col-4 bg-light text-right">
+            <input type="checkbox" checked data-toggle="toggle" data-onstyle="secondary" data-size="xs" data-onstyle="success">
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- https://gitbrent.github.io/bootstrap4-toggle/ -->
+    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"></script>
 
   </body>
 </html>
