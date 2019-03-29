ba6a31c6349ab76d7e3d98b156007bfd84f2c265
Alessandro Cuoghi <agileontheweb@gmail.com>
2019-03-29 21:38:23 +0100

    some change

--- js/layout.js
+++ js/layout.js
@@ -1,28 +1,29 @@
 $( document ).ready(function() {
   console.log( "ready!" );
-  temperatureRaspberry();
   var localhost = 'http://192.168.43.202/golia-camper';
   console.log(localhost);
+  temperatureRaspberry(localhost);
 });
 
 
-function temperatureRaspberry(){
-  console.log("temperature-rasp");
+function temperatureRaspberry(localhost){
+  console.log("temperature-rasp" + localhost);
 
   $( "#btn-raspberry-temp" ).click(function() {
         $.ajax({
         type:'get',
-        url: localhost + '/cgi-bin/temperature-raspberry.py',
+        url: '/cgi-bin/temperature-raspberry.py',
         cache:false,
         success: function(data) {
           console.log(data);
-          setInterval(function(){
-            $("#temperature-cpu").load(localhost + '/golia-camper/php/raspberry-temp.php');
-          }, 1000);
         },
         error: function(request, status, error) {
           console.log("failed");
         }
      });
   });
+
+  setInterval(function(){
+    $("#temperature-cpu").load(localhost + '/php/raspberry-temp.php');
+  }, 1000);
 }
--- php/raspberry-temp.php
+++ php/raspberry-temp.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen(localhost + "/txt/raspberry-temp.txt", "r") or die("Unable to open file!");
-  echo fread($myfile,filesize(localhost + "/txt/raspberry-temp.txt"));
+  $myfile = fopen(localhost + "golia-camper/txt/raspberry-temp.txt", "r") or die("Unable to open file!");
+  echo fread($myfile,filesize(localhost + "golia-camper/txt/raspberry-temp.txt"));
   fclose($myfile);
 ?>
