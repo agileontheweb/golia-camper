eeb849fd05ac7dc50cbbdfae80545df0d4187a6e
Alessandro Cuoghi <agileontheweb@gmail.com>
2019-03-29 19:08:24 +0100

    layout js

--- index.php
+++ index.php
@@ -16,13 +16,12 @@
     <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/css/bootstrap4-toggle.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
     <title>GOLIATH - Panel control</title>
   </head>
   <body>
 
     <div class="container">
-      <div class="row align-items-center py-2 bg-dark text-white">
+      <div class="row align-items-center py-2 bg-dark text-white system">
         <div class="col-4">
           <small>
             <i class="fas fa-car-battery"></i> Bs 12 V
@@ -33,9 +32,9 @@
             <i class="fas fa-car-battery"></i> Bm 12 V
           </small>
         </div>
-        <div class="col-4">
+        <div class="col-4 raspberry-thermometer">
           <small>
-            <i class="fas fa-thermometer-full"></i> Rasp 23
+            <i class="fas fa-thermometer-full"></i> Rasp <span></span>
           </small>
         </div>
       </div>
@@ -127,14 +126,23 @@
 
     </div>
 
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
 
     <!-- https://gitbrent.github.io/bootstrap4-toggle/ -->
     <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"></script>
+
+    <!-- My script -->
+    <script type="text/javascript" src="js/layout.js"></script>
+    <!-- <script>
+    $( document ).ready(function() {
+        console.log( "ready!" );
+    });
+  </script> -->
 
   </body>
 </html>
