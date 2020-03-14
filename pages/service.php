947c39021b27ad7b75e6265ba677e2dee4a062b8
Alessandro Cuoghi <agileontheweb@gmail.com>
2020-03-14 01:13:36 +0100

    save name file

--- pages/service.php
+++ pages/service.php
@@ -3,33 +3,34 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/style.css" crossorigin="anonymous">
-    <title>GOLIATH - Panel control</title>
+    <link rel="stylesheet" href="../css/style.css" crossorigin="anonymous">
+    <title>GOLIATH - Service</title>
   </head>
   <body>
 
   <div class="flex content-start bg-gray-200">
     <div class="w-1/3 border">
       <div class="text-gray-700 text-center bg-gray-400 p-10">
-        <a href="pages/camera.php">Telecamere</a>
+        <a href="../">Home</a>
       </div>
     </div>
     <div class="w-1/3 border">
       <div class="text-gray-700 text-center bg-gray-400 p-10">
-        <a href="pages/service.php">Servizi</a>
+        Portellone
+      </div>
+    </div>
+    <div class="w-1/3 border">
+      <div class="text-gray-700 text-center bg-gray-400 p-10">
+        Dinette
       </div>
     </div>
 
     <div class="w-1/3 border">
       <div class="text-gray-700 text-center bg-gray-400 p-10">
-        <a href="pages/radio.php">Radio</a>
+        etcc..
       </div>
     </div>
   </div>
-
-  <!-- <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"></script>
-  <script type="text/javascript" src="js/layout.js"></script> -->
 
 </body>
 </html>
