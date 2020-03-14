31dd79b552d21ef37f4bd787f8326b90753147db
Alessandro Cuoghi <agileontheweb@gmail.com>
2020-03-14 02:00:19 +0100

    Add some new buttons

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
   </head>
   <body>
 
+  <div role="alert">
+    <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+      Porta aperta
+    </div>
+  </div>
+
   <div class="flex content-start bg-gray-200">
     <div class="w-1/3 border">
       <a href="pages/camera.php">
@@ -24,7 +30,6 @@
       </a>
     </div>
 
-
     <div class="w-1/3 border">
       <a href="pages/radio.php">
         <div class="text-gray-700 text-center bg-gray-400 p-10">
@@ -34,6 +39,15 @@
     </div>
   </div>
 
+  <div class="absolute bottom-0 left-0 right-0">
+    <div class="border">
+      <a href="#">
+        <div class="text-gray-400 text-center bg-gray-800">
+          Test Goliath
+        </div>
+      </a>
+    </div>
+  </div>
   <!-- <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"></script>
   <script type="text/javascript" src="js/layout.js"></script> -->
--- pages/service.php
+++ pages/service.php
@@ -16,29 +16,99 @@
         </div>
       </a>
     </div>
-    <div class="w-1/3 border">
-      <a href="#">
-        <div class="text-gray-700 text-center bg-gray-400 p-10">
-          Portellone
-        </div>
-      </a>
-    </div>
-    <div class="w-1/3 border">
-      <a href="#">
-        <div class="text-gray-700 text-center bg-gray-400 p-10">
-          Dinette
-        </div>
-      </a>
-    </div>
+  </div>
 
+  <div class="flex bg-gray-200">
+    <div class="flex">
+      <p>Servizi</p>
+    </div>
+  </div>
+
+  <div class="flex">
     <div class="w-1/3 border">
       <a href="#">
         <div class="text-gray-700 text-center bg-gray-400 p-10">
-          etcc..
+          220
+        </div>
+      </a>
+    </div>
+    <div class="w-1/3 border">
+      <a href="#">
+        <div class="text-gray-700 text-center bg-gray-400 p-10">
+          Profumo
         </div>
       </a>
     </div>
   </div>
+
+
+  <div class="flex bg-gray-200">
+    <div class="flex">
+      <p>Luci</p>
+    </div>
+  </div>
+
+  <div class="flex">
+    <div class="flex">
+      <div class="w-1/3 border">
+        <a href="#">
+          <div class="text-gray-700 text-center bg-gray-400 p-10">
+            Cucina
+          </div>
+        </a>
+      </div>
+
+
+      <div class="w-1/3 border">
+        <a href="#">
+          <div class="text-gray-700 text-center bg-gray-400 p-10">
+            Dinette
+          </div>
+        </a>
+      </div>
+
+      <div class="w-1/3 border">
+        <a href="#">
+          <div class="text-gray-700 text-center bg-gray-400 p-10">
+            Dietro
+          </div>
+        </a>
+      </div>
+
+      <div class="w-1/3 border">
+        <a href="#">
+          <div class="text-gray-700 text-center bg-gray-400 p-10">
+            Cucina
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="flex bg-gray-200">
+    <div class="flex">
+      <p>Batterie</p>
+    </div>
+  </div>
+
+  <div class="flex">
+    <div class="w-1/3 border">
+      <a href="#">
+        <div class="text-gray-700 text-center bg-gray-400 p-10">
+          12V Servizi
+        </div>
+      </a>
+    </div>
+    <div class="w-1/3 border">
+      <a href="#">
+        <div class="text-gray-700 text-center bg-gray-400 p-10">
+          12V Motore
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
