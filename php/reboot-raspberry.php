2fd200c27b93dbe160d522c80fca431c4a81d26e
Alle <agileontheweb@gmail.com>
2020-03-21 23:15:57 +0100

    reboot php python from web

--- php/reboot-raspberry.php
+++ php/reboot-raspberry.php
@@ -1,4 +1,12 @@
 <?php
-  echo "Riavvio il raspberry";
-  exec('sudo python /var/www/html/golia-camper/py/reboot-raspberry.py');
+  echo "1Riavvio il raspberry";
+  //exec("python /var/www/reboot.py"
+  //exec("sudo python /var/www/html/golia-camper/py/reboot-raspberry.py")
+    echo "Current user is: " . get_current_user();
+    echo "Command output is " . shell_exec("python /var/www/html/golia-camper/py/reboot-raspberry.py");
+
+
+//  exec('sudo python /usr/lib/cgi-bin/reboot.py');
+  // system('sudo shutdown -r now');
+  // system('sudo /sbin/shutdown -h now');
 ?>
--- py/reboot-raspberry.py
+++ py/reboot-raspberry.py
@@ -1,2 +1,5 @@
-import os
-os.system('sudo shutdown -r now')
+command = "/usr/bin/sudo /sbin/reboot"
+import subprocess
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print output
