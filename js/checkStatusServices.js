b1b65756f243f890bceedf3e3a85c235ce60b305
Alle <agileontheweb@gmail.com>
2020-03-22 18:13:48 +0100

    add push button

--- js/checkStatusServices.js
+++ js/checkStatusServices.js
@@ -1,17 +1,38 @@
 function checkStatusServices(){
   var listButtons = ["camera-front",
-                      "camera-back",
-                      "camera-water-discharge",
-                      "light-back",
-                      "light-dinette",
-                      "light-kitchen",
-                      "light-left",
-                      "perfume"]
+                    "camera-back",
+                    "camera-water-discharge",
+                    "light-back",
+                    "light-dinette",
+                    "light-kitchen",
+                    "light-left",
+                    "perfume"]
 
-  $.each( listButtons, function( key, value ) {
-    $.get('txt/' + value + '.txt', function(data) {
-      onOff(value,data)
+  $.each( listButtons, function( key, id ) {
+    $.get('txt/' + id + '.txt', function(data) {
+
+      status_text = data;
+      console.log("init checkStatusServices: " + status_text);
+
+      $.ajax({
+        type:'post',
+        url: '/golia-camper/php/onoff.php',
+        cache: 'false',
+        data: {
+          'status_text' : status_text, 'id': id},
+          dataType: "text",
+          success: function(data) {
+            console.log(data, id);
+          },
+          error: function(request, status, error) {
+          console.log("failed");
+        }
+      });
+
+      onOff(id,data);
     });
+
   });
   $('#generic-notice').hide();
 }
+
